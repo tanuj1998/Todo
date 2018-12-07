63867d01d3728a5dc2a747cd7de2cf7cdae480ec
tanuj1998 <tyadav@purdue.edu>
2018-12-07 03:36:05 -0500

    Added security

--- app/app.jsx
+++ app/app.jsx
@@ -11,18 +11,43 @@ import TodoApp from 'TodoApp';
 var Api = require('Api');
 
 import Login from 'Login'
+import firebase from 'app/firebase/index';
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    hashHistory.push('/todos');
+  } else {
+    hashHistory.push('/');
+  }
+});
 
 store.dispatch(actions.startAddTodos());
 require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 require('style!css!sass!applicationStyles')
+
+var requireLogin = (nextState, replace, next) => {
+  if (!firebase.auth().currentUser) {
+    replace('/');
+  }
+  next();
+};
+
+var redirectIfLoggedIn = (nextState, replace, next) => {
+  if (firebase.auth().currentUser) {
+    replace('/todos');
+  }
+
+  next();
+};
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={hashHistory}>
       <Route path="/">
-        <Route path="todos" component={TodoApp}/>
-        <IndexRoute component={Login}/>
+        <Route path="todos" component={TodoApp} onEnter={requireLogin}/>
+        <IndexRoute component={Login} onEnter={redirectIfLoggedIn}/>
       </Route>
     </Router>
   </Provider>,
