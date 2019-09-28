8c053a78bb459e98a92e8aa383b6ab790e0473ce
tanuj1998 <tyadav@purdue.edu>
2019-09-28 16:28:58 -0400

    Update index.js

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,17 +2,16 @@ import firebase from 'firebase';
 
 try{
   var config = {
-      apiKey: "AIzaSyC6hTa0p7F1yOoujW_R7-w13vkE_QABk1Y",
-      authDomain: "finalapp-f6c61.firebaseapp.com",
-      databaseURL: "https://finalapp-f6c61.firebaseio.com",
-      storageBucket: "finalapp-f6c61.appspot.com",
-      projectId: "finalapp-f6c61",
-      messagingSenderId: "54214171726"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      storageBucket: "",
+      projectId: "",
+      messagingSenderId: ""
     };
     firebase.initializeApp(config);
 }
 catch(e){
-
 }
   export var githubCheck = new firebase.auth.GithubAuthProvider();
 
