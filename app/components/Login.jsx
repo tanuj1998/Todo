8d00671b31b31ed3a3da61dcdc105ca9fa7c2899
tanuj1998 <tyadav@purdue.edu>
2019-10-05 15:39:12 -0400

    Update Login.jsx

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -15,7 +15,6 @@ export var Login = React.createClass({
           <div className="columns small-centered small-10 medium-6 large-4">
             <div className="callout callout-auth">
               <h3>Login</h3>
-              
               <p>
                 Login with GitHub account below.
               </p>
